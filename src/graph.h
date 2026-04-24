84cef52a0e005124868f711e51ce0a9dcf19c65c
Milán Várady <varady.misi@gmail.com>
2026-04-24 21:54:53 +0200

    Implement Graph<T>::Queue

--- src/graph.h
+++ src/graph.h
@@ -5,7 +5,9 @@
 
 #include <cstddef>
 #include <ostream>
+#include <stdexcept>
 #include <string>
+#include <type_traits>
 
 template<typename T>
 class Graph {
@@ -44,7 +46,7 @@ private:
         size_t capacity;
 
     public:
-        Queue(size_t);
+        Queue(size_t capacity);
         ~Queue();
 
         void enqueue(size_t idx);
@@ -52,3 +54,37 @@ private:
         bool isEmpty() const;
     };
 };
+
+// Queue implementation
+
+template<typename T>
+Graph<T>::Queue::Queue(size_t capacity) : head(0), tail(0), capacity(capacity + 1) {
+    this->data = new size_t[capacity];
+}
+
+template<typename T>
+Graph<T>::Queue::~Queue() {
+    delete[] this->data;
+}
+
+template<typename T>
+void Graph<T>::Queue::enqueue(size_t idx) {
+    this->data[this->tail] = idx;
+    this->tail = (this->tail + 1) % this->capacity;
+}
+
+template<typename T>
+size_t Graph<T>::Queue::dequeue() {
+    if (this->isEmpty())
+        throw std::out_of_range("Queue is empty");
+
+    size_t head_data = this->data[head];
+    this->head = (this->head + 1) % this->capacity;
+
+    return head_data;
+}
+
+template<typename T>
+bool Graph<T>::Queue::isEmpty() const {
+    return this->head == this->tail;
+}

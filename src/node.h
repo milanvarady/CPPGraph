ea73ab101aaafadd66ce1d5a5e387983f18f493c
Milán Várady <varady.misi@gmail.com>
2026-04-24 20:43:55 +0200

    Create Node implementation

---
+++ src/node.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <iostream>
+#include <ostream>
+
+template<typename T>
+class Node {
+    T data;
+
+public:
+    Node(const T&);
+    Node& operator=(const Node&);
+    bool operator==(const Node<T>&) const;
+    const T& getData() const;
+    void setData(const T&);
+};
+
+template<typename T>
+Node<T>::Node(const T& data) : data(data) {}
+
+template<typename T>
+Node<T>& Node<T>::operator=(const Node<T>& other) {
+    this->data = other.data;
+
+    return *this;
+}
+
+template<typename T>
+bool Node<T>::operator==(const Node<T>& other) const {
+    return this->data == other.data;
+}
+
+template<typename T>
+const T& Node<T>::getData() const {
+    return this->data;
+}
+
+template<typename T>
+void Node<T>::setData(const T& data) {
+    this->data = data;
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const Node<T>& node) {
+    os << node.getData();
+    return os;
+}

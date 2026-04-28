eb860d6137d5c28b7b2aa0379970eed6eb98302c
Milán Várady <varady.misi@gmail.com>
2026-04-28 16:49:39 +0200

    Add Doxide config

---
+++ build_docs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# Generate API docs from source
+doxide build
+
+# Copy specification and assets into doxide output
+cp docs/specification.md doxide/
+cp -r docs/uml-diagram doxide/
+
+# Build the site
+mkdocs build
